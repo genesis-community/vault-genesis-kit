2afb08e7a286a87f6759aeb9dc27b6a10b3c986d
ramonskie <ramonmakkelie@gmail.com>
2020-09-02 13:46:28 +0200

    small changes

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Concourse Kit", func() {
 		Test(Environment{
 			Name:        "azure",
 			CloudConfig: "aws",
-			CPI:         "azure", //TODO: fix cpi
+			CPI:         "azure",
 		})
 	})
 })
