84e030e98abadc991b0006f262d9a61e8d11915c
Kevin Rutten <krutten@fivetwenty.io>
2025-09-08 21:06:33 +0000

    Update New hook for compile error

--- hooks/new.pm
+++ hooks/new.pm
@@ -6,7 +6,7 @@ use warnings;
 # Only needed for development
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
 
-use parent qw(Genesis::Hook::New);
+use parent qw(Genesis::Hook);
 
 use Genesis qw/bail/;
 use Genesis::UI qw/prompt_for_boolean/;
